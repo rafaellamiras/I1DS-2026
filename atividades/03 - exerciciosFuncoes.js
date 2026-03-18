03d907a08bef389bff213ad2abbeb3e19eaa6a12
rafaellamiras <rafaellamiras@gmai.com>
2026-03-18 08:16:57 -0300

    Atividade de funções!

--- atividades/03 - exerciciosFuncoes.js
+++ atividades/03 - exerciciosFuncoes.js
@@ -9,19 +9,36 @@ Lista de Exercícios
  veículo faz uma média de 12 Km/L.
 */
 
+console.log("===================IDADE===============================");
+
 function subtrair(anoAtual, anoNascimento) {
   let idadeAtual = anoAtual - anoNascimento;
   return idadeAtual;
 }
 
-
 let anoAtual = 2026;
 let anoNascimento = 2010;
 
-console.log("__________________________________________________")
-console.log("2026 - 2010=", subtrair(anoAtual, anoNascimento));
-console.log("A idade atual da pessoa que nasceu em 2010 é:", subtrair(anoAtual, anoNascimento));
-console.log("__________________________________________________")
+console.log("__________________________________________________");
+console.log("2026 - 2010 =", subtrair(anoAtual, anoNascimento));
+console.log(
+  "A idade atual da pessoa que nasceu em 2010 é:",
+  subtrair(anoAtual, anoNascimento),
+);
+console.log("__________________________________________________");
 
+console.log("===================TRAJETO===============================");
 
-function multiplicacao(quantidadeCombustível, )
+function multiplicar(quantCombustivel, kilometros) {
+  let kmViagem = quantCombustivel * kilometros;
+  return kmViagem;
+}
+
+let quantCombustivel = 40;
+let kilometros = 12;
+
+console.log("__________________________________________________");
+console.log("Com 40 litros de comsbustível, quantos km ele pode percorrer?");
+console.log("40 * 12 =", multiplicar(quantCombustivel, kilometros));
+console.log("Ele percorre 480 kilometros com 40 litros!");
+console.log("__________________________________________________");
