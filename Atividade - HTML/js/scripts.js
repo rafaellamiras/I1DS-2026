e0ed924c06c099b1e7b00bb58644b4937414b976
rafaellamiras <rafaellamiras@gmail.com>
2026-05-12 13:37:26 -0300

    atividade complementar - formulário, tabela e aplicação de desconto

--- Atividade - HTML/js/scripts.js
+++ Atividade - HTML/js/scripts.js
@@ -49,3 +49,118 @@ const excluir = (id) => {
     }
   });
 };
+
+/*let total = 0;
+
+function adicionarProduto() {
+  let produto = document.getElementById("produto").value;
+  let quantidade = Number(document.getElementById("quantidade").value);
+  let valor = Number(document.getElementById("valor").value);
+
+  if (produto === "" ||
+    isNaN(quantidade) ||
+    isNaN(valor)
+    
+  ){
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+let totalProduto = quantidade * valor
+let tabelaProdutos = document.querySelector("#tabelaProdutos tbody")
+let linha = document.createElement("tr")
+
+    linha.innerHTML = `
+        <td>${produto}</td>
+        <td>${quantidade}</td>
+        <td>R$ ${valor.toFixed(2)}</td>
+        <td>R$ ${totalProduto.toFixed(2)}</td>
+    `;
+    tabelaProdutos.appendChild(linha);
+
+
+    // SOMAR TOTAL GERAL
+    total += totalProduto;
+
+    // MOSTRAR TOTAL
+    document.getElementById("total").innerText =
+        total.toFixed(2);
+
+    // MOSTRAR VALOR LIQUIDO
+    document.getElementById("valorLiquido").innerText =
+        total.toFixed(2);
+
+    // LIMPAR CAMPOS
+    document.getElementById("produto").value = "";
+    document.getElementById("quantidade").value = "";
+    document.getElementById("valor").value = "";
+}
+*/
+
+let produtos = [];
+let totalGeral = 0;
+
+const adicionarProduto = () => {
+  let nome = document.getElementById("produto").value;
+  let qtd = parseInt(document.getElementById("quantidade").value);
+  let valor = parseFloat(document.getElementById("valor").value);
+
+  if (!nome || !qtd || !valor) {
+    alert("Preencha todos os campos");
+    return;
+  }
+  let total = qtd * valor;
+  produtos.push({
+    nome,
+    qtd,
+    valor,
+    total,
+  });
+  atualizarTabela();
+  document.getElementById("produto").value = "";
+  document.getElementById("quantidade").value = "";
+  document.getElementById("valor").value = "";
+};
+
+const atualizarTabela = () => {
+  let tbody = document.querySelector("#tabelaProdutos tbody");
+  tbody.innerHTML = "";
+  produtos.forEach((item) => {
+    tbody.innerHTML += `
+      <tr>
+        <td>${item.nome}</td>
+        <td>${item.qtd}</td>
+        <td>${item.valor.toFixed(2)}</td>
+        <td>${item.total.toFixed(2)}</td>
+      </tr>
+    `;
+  });
+};
+
+
+
+const finalizarCompra = () => {
+  totalGeral = 0;
+  produtos.forEach((item) => {
+    totalGeral += item.total;
+  });
+  document.getElementById("total").innerText = totalGeral.toFixed(2);
+  document.getElementById("valorLiquido").innerText = totalGeral.toFixed(2);
+};
+
+
+const aplicarDesconto = () => {
+  let descontoValor =
+    parseFloat(document.getElementById("descontoValor").value) || 0;
+
+    let descontoPercentual =
+    parseFloat(document.getElementById("descontoPercentual").value) || 0;
+  let valorFinal = totalGeral;
+  if (descontoValor > 0) {
+    valorFinal -= descontoValor;
+  }
+  if (descontoPercentual > 0) {
+    valorFinal -= totalGeral * (descontoPercentual / 100);
+  }
+  document.getElementById("valorLiquido").innerText = valorFinal.toFixed(2);
+};
